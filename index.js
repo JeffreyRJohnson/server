ae3150f9cfde929752b57805a56661274cb5d922
Jeff <jeffreyrjohnsonsemail@gmail.com>
2017-11-23 18:00:35 -0600

    minor change for testing

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/',(req,res)=> {
-res.send({hi: 'there'});
+app.get("/", (req, res) => {
+  res.send({ bye: "buddy" });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000);
-

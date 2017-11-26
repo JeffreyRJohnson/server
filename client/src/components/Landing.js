c01410d21449c3e0ed9ddce2955e6b65d3ea9bd4
Jeff <jeffreyrjohnsonsemail@gmail.com>
2017-11-26 17:54:10 -0600

    Added billing and client side app!

---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    //  <div style={{ textAlign: 'center' }}>
+    <div className="center-align">
+      <h1>Emaily!</h1>
+      Collect feedback from your users
+    </div>
+  );
+};
+
+export default Landing;

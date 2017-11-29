e67251651bbd86bdef06a0005bcb0201908aa6e7
Jeff <jeffreyrjohnsonsemail@gmail.com>
2017-11-29 14:19:23 -0600

    finished app

--- config/prod.js
+++ config/prod.js
@@ -6,6 +6,6 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY,
+  sendGridProdKey: process.env.SEND_GRID_PROD_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN
 };
